274565a695fe921b3850c1ef69d5ce617286f6e7
JuanMolfese <juanjosemolfese@gmail.com>
2022-05-21 11:11:03 -0300

    se implementa logo.svg a formato jsx

--- TP2/entregafinal/components/navbar.js
+++ TP2/entregafinal/components/navbar.js
@@ -1,6 +1,7 @@
 import {FaBars, FaUserAlt} from 'react-icons/fa';
 import Image from 'next/image';
 import styles from '../styles/navbar.module.css';
+import Logo from './logo';
 
 export function Navbar(){
   return (
@@ -8,6 +9,7 @@ export function Navbar(){
     <nav>
       <FaBars className={styles.navbarIcon}/>
       {/*<Image src="/favicon.ico" alt="logo" width={50} height={50} />*/}
+      <Logo></Logo>
       <Image src="/logo.png" alt="logo CM Games" width={42} height={46}/>
       <FaUserAlt />
     </nav>
