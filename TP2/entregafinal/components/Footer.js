dc4fa24f94dec30d320df34d63a2ca785355d2e6
fdcrespi <fdcrespi@gmail.com>
2022-05-25 12:44:27 -0300

    Se arreglo la api

---
+++ TP2/entregafinal/components/Footer.js
@@ -0,0 +1,10 @@
+import styles from '../styles/footer.module.css';
+
+export function Footer(){
+  return (
+    <footer className={styles.footer}>
+        <h4>Copyright CM software</h4>
+    </footer>
+  )
+}
+export default Footer;
