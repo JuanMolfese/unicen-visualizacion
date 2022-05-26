7a6919691b244be3575d163b04da16e5fb297ea9
Juan <juanjosemolfese@gmail.com>
2022-05-26 00:11:53 -0300

    Se termina primer etapa de maquetado del componente GameCard y su css. Cuando este lista la api, hay q integrar bien.

--- TP2/entregafinal/components/GameCard.js
+++ TP2/entregafinal/components/GameCard.js
@@ -1,24 +1,28 @@
 import Image from 'next/image';
+import styles from '../styles/GameCard.module.css';
 
-/* lo llamaria
-    <div>
+/* Donde se consuma esta, importar: 
+import styles from '../styles/GameCard.module.css';
+o definir en el css q se use ahi como vamos a acomodar cada card generada por el .map
+
+luego pondria: 
+
+    <div className="gameContainer">
         {games.map((game) => (
             <GameCard game={game} key={game.id}/>
     </div>
     ))}
-     */
+*/
+
 export default function GameCard({ game }){
     return(
-        <section>
+        <section className={styles.card}>
             <Link href="../pages/game/{game.id}">
-                <a><Image src={game.thumbnail} width="200" height="200"/></a>
+                <a><Image src={`/${game.thumbnail}`} width="200" height="200"/></a>
             </Link>
             <div>
-                <h2>{game.title}</h2>
-                <p></p>
+                <h4>{game.title}</h4>                
             </div>
         </section>
-
-
     )
 }
