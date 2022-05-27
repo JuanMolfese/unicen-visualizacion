c6c4c38d66e144479feda5e8797eafa9a9c821b8
JuanMolfese <juanjosemolfese@gmail.com>
2022-05-26 23:40:50 -0300

    arreglo defualt profile

--- TP2/entregafinal/pages/profile.js
+++ TP2/entregafinal/pages/profile.js
@@ -3,7 +3,7 @@ import { FaWindowClose } from "react-icons/fa";
 import Link from "next/link";
 
 
-export function Profile(){
+export default function Profile(){
     return (
         <>
             <div className={styles.container}>
