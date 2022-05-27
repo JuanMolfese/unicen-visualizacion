b99e30a4e6a13f6374d965ccf13e0e53c5669e63
JuanMolfese <juanjosemolfese@gmail.com>
2022-05-27 14:17:14 -0300

    ajuste css en game.module e [id], tamaño video, tamaño titulo y margin left del box del share y like. Tambien se agregan dos lineas <meta> para que ajuste formato APP al "instalar" en el escritorio de smartphone.

--- TP2/entregafinal/pages/games/[id].js
+++ TP2/entregafinal/pages/games/[id].js
@@ -67,7 +67,7 @@ export default function Page({game}) {
             <FaHeart className={styles.iconLike}/>
           </div>
         </div>
-        <iframe className={styles.imgGame} width="100%" height="315" src={`${game.video_url}?autoplay=1`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe className={styles.imgGame} width="100%" height="250" src={`${game.video_url}?autoplay=1`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <p className={styles.descriptionGame}>{game.short_description}</p>
         <Link href="/play_game">
           <a><button className={styles.btnPlay}>¡ Jugar !</button></a>
