6aadc3202f0266968f0746d97677d7555c633708
fdcrespi <fdcrespi@gmail.com>
2022-05-28 17:01:28 -0300

    Se empezó la edicion del profile

--- TP2/entregafinal/pages/profile.js
+++ TP2/entregafinal/pages/profile.js
@@ -1,17 +1,40 @@
 import styles from "../styles/Profile.module.css";
-import { FaWindowClose } from "react-icons/fa";
 import Link from "next/link";
+import Navbar from "../components/navbar";
+import {FiLogOut} from 'react-icons/fi'
 
 
-export default function Profile(){
+export default function Profile(genres){
+
     return (
         <>
+            <Navbar genres={genres}></Navbar>
             <div className={styles.container}>
-                
-                <Link href="/">
-                    <FaWindowClose className={styles.iconClose}/>
-                </Link>                
+                <div className={styles.logout}>
+                    <Link href="/">
+                        <FiLogOut className={styles.iconClose}/>
+                    </Link>                
+                </div>
+                <div className={styles.profile}>
+                    <div className={styles.profile_image}>
+                        <img width={50} height={50} src="https://avatars3.githubusercontent.com/u/56955891?s=460&u=f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8&v=4" alt="profile"/>
+                        <p>juanmolfese@gmail.com</p>
+                    </div>
+                </div>
+
             </div>
         </>
     )
 }
+
+export const getServerSideProps = async () => {
+    const resGenres = await fetch('https://unicen-visualizacion-juanmolfese.vercel.app/api/categories');
+    const genres = await resGenres.json();
+    return {
+      props: {
+        // props that you want to pass to the page
+        genres: genres,
+      },
+    };
+  }
+  
