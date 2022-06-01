2890c6790b02e3fc00c6db14443b026d6ef0ce6c
fdcrespi <fdcrespi@gmail.com>
2022-06-01 14:24:21 -0300

    Se cambio el home en desktop

--- TP2/entregafinal/pages/index.js
+++ TP2/entregafinal/pages/index.js
@@ -19,22 +19,32 @@ export default function Home({games, genres, promotion, favs}) {
   return (
     <>
       <Navbar genres={genres}></Navbar>
-      <Link href="/games/[id]" as={`/games/${promotion.id}`}>
-        <section className={styles.card}>
-          <h6 className={styles.juegoPromocionado}>Juego promocionado</h6>
-            <img src={promotion.thumbnail} width="100%vh" height="auto"/>
-          <h4 className={styles.gameTitle}>{promotion.title}</h4>                
-        </section>
-      </Link>
-      {(session) ?
-        <div className={styles.listFav}>
-          <h3 className={styles.groupGame}>Tus Favoritos</h3>
-          <ListGames games={favs}></ListGames>
-        </div>
-        : null
-      }
-      <h3 className={styles.groupGame}>Todos los juegos</h3>
-      <ListGames games={games}></ListGames>
+      <main className='main'>
+        <Link href="/games/[id]" as={`/games/${promotion.id}`}>
+          <section className={styles.card}>
+            <h6 className={styles.juegoPromocionado}>Juego promocionado</h6>
+            <div className={styles.contentGame}>
+              <img src={promotion.thumbnail} width="100%vh" height="auto"/>
+              <ul className={styles.contentGameText}>
+                <li> <span>Descripción:</span> {promotion.short_description}</li>
+                <li> <span>Categoria:</span> {promotion.genre}</li>
+                <li> <span>Fecha de creacion: </span> {promotion.release_date} </li>
+                <li> <span>Desarrolado por: </span> {promotion.developer} </li>
+              </ul>
+            </div>
+            <h4 className={styles.gameTitle}>{promotion.title}</h4>                
+          </section>
+        </Link>
+        {(session) ?
+          <div className={styles.listFav}>
+            <h3 className={styles.groupGame}>Tus Favoritos</h3>
+            <ListGames games={favs}></ListGames>
+          </div>
+          : null
+        }
+        <h3 className={styles.groupGame}>Todos los juegos</h3>
+        <ListGames games={games}></ListGames>
+      </main>
       <Advertising></Advertising>
       <Footer></Footer>
     </>
