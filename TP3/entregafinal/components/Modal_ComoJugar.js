f4084dc1d025f81994d7a297960f7179d66e5212
JuanMolfese <juanjosemolfese@gmail.com>
2022-06-12 14:38:56 -0300

    .

--- TP3/entregafinal/components/Modal_ComoJugar.js
+++ TP3/entregafinal/components/Modal_ComoJugar.js
@@ -19,11 +19,16 @@ export default function Modal_ComoJugar(){
             <FaTimes className={styles.iconClose} onClick={closeModal}/>
         </div>
         <div className={styles.container_detalle_como_jugar}>
-            <span className={styles.titulo_como_jugar}>Como Jugar al Pacman ?</span>
+            <span className={styles.titulo_como_jugar}>Como Jugar al 4 en linea ?</span>
             <br/><br/>
-            <span>Si jugas desde una PC: Usa las flechas del teclado para mover al personaje.</span>
-            <br/><br/>
-            <span>Si jugas desde un Smartphone: Desliza el dedo sobre la pantalla para mover al personaje</span>
+            <span>Quizás una de las principales razones de la popularidad del juego es su simplicidad. Si bien no hay reglas complicadas que seguir, existen estrategias para aumentar sus posibilidades de ganar.
+              Una vez que cada jugador elige un color, generalmente rojo o amarillo, uno de ellos comienza colocando un disco en cualquier lugar de la cuadrícula vertical de siete columnas y seis filas, que se usa para jugar. Cuando un jugador coloca un disco, la única forma de detenerlo es con otro disco.
+              El objetivo del juego es conectar cuatro discos de un mismo color de cualquiera de las tres formas siguientes:
+              Verticalmente, Horizontalmente y Diagonalmente.
+              Puedes jugar Connect 4 en diferentes variaciones. La cantidad de piezas necesarias para conectarse para ganar puede variar. Algunas versiones requieren una rejilla más grande, lo que permite la conexión de cinco o incluso seis piezas.
+              Existe la posibilidad de que no todos los juegos terminen con una victoria para uno de los jugadores. Si se llenan todas las celdas de la cuadrícula y no hay cuatro discos conectados, se anuncia un sorteo sin ganadores.
+            </span>
+            
         </div> 
       </div>
     </>
