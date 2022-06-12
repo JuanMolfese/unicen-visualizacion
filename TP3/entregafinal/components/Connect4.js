5df26fc8261c940d34ed00a0026dda0ba71fc36a
JuanMolfese <juanjosemolfese@gmail.com>
2022-06-12 17:39:57 -0300

    se elimina pacman.gif y se coloca un backgound, se genera componente del juego Connect4.js

---
+++ TP3/entregafinal/components/Connect4.js
@@ -0,0 +1,12 @@
+
+
+export default function Connect4() { 
+
+    return (
+        <>
+
+
+
+        </>
+    )
+}
--- TP3/entregafinal/pages/play_game.js
+++ TP3/entregafinal/pages/play_game.js
@@ -1,11 +1,19 @@
 import { Navbar_inGame } from "../components/NavBar_inGame";
 import Footer from "../components/Footer";
+import styles from "../styles/playGame.module.css";
+import Connect4 from "../components/Connect4";
 
 export function PlayGame(){
     return(
         <>
             <Navbar_inGame></Navbar_inGame>
-            <img src="/pacman.gif" width="100%" height="500px"></img>                   
+            
+            <div className={styles.playGameContainer}>
+                <img src="./backgrounds/3.jpg" className={styles.background}></img>
+            </div>
+            
+            <Connect4></Connect4> {/*  JUEGO */}
+
             {/* <Footer></Footer> */}
         </>
     )
