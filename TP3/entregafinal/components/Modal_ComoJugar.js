258cddb93a5701cefc068de740aee12d704a5dcd
JuanMolfese <juanjosemolfese@gmail.com>
2022-06-20 18:31:33 -0300

    interlineado como jugar

--- TP3/entregafinal/components/Modal_ComoJugar.js
+++ TP3/entregafinal/components/Modal_ComoJugar.js
@@ -21,7 +21,7 @@ export default function Modal_ComoJugar(){
         <div className={styles.container_detalle_como_jugar}>
             <span className={styles.titulo_como_jugar}>Como Jugar al 4 en linea ?</span>
             <br/><br/>
-            <span> El objetivo del juego es conectar cuatro fichas de un mismo color de cualquiera de las tres formas siguientes:
+            <span className={styles.textoComoJugar}> El objetivo del juego es conectar cuatro fichas de un mismo color de cualquiera de las tres formas siguientes:
               Verticalmente, Horizontalmente y Diagonalmente.
               Puedes jugar Connect 4 en diferentes variaciones. La cantidad de piezas necesarias para conectarse para ganar puede variar. Algunas versiones requieren una rejilla más grande, lo que permite la conexión de cinco o incluso seis piezas.
               Existe la posibilidad de que no todos los juegos terminen con una victoria para uno de los jugadores. Si se llenan todas las celdas de la cuadrícula y no hay cuatro discos conectados, se anuncia un sorteo sin ganadores.
