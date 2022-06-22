b6a54bbb887ffee3fd8a462edf4a66b6ecf85768
fdcrespi <fdcrespi@gmail.com>
2022-06-22 11:29:21 -0300

    Se agregaron comentarios a funciones

--- TP3/entregafinal/pages/play_game.js
+++ TP3/entregafinal/pages/play_game.js
@@ -71,20 +71,21 @@ export function PlayGame(){
 
         let board = [];
 
-
+        /* Llama al crear ficha y la dibuja */
         function addFigure(color, jug, active) {
-        addCircle(color, jug, active);
-        drawFigure();
+            addCircle(color, jug, active);
+            drawFigure();
         }
 
         /* Borra el canvas y redibuja todo */
         function drawFigure() {
-        clearCanvas();
-        for (let i = figures.length - 1; i > 0; i--) {
-            figures[i].draw();
-        }
+            clearCanvas();
+            for (let i = figures.length - 1; i > 0; i--) {
+                figures[i].draw();
+            }
         }
 
+        /* Dibuja la base para el tablero */
         function drawTab(){
             /* dibujo base tablero */
             ctx.beginPath();
@@ -93,41 +94,42 @@ export function PlayGame(){
 
         }
 
-
+        /* Vacia el canvas para que sea redibujado con cada evento */
         function clearCanvas() {
-        //ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-        /* ctx.fillStyle = "#BC71BC"; */
-        /* ctx.fillRect(0, 0, canvasWidth, canvasHeight); */
-        ctx.clearRect(0, 0, canvasWidth, canvasHeight); 
-        addTablero();
+            //ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+            /* ctx.fillStyle = "#BC71BC"; */
+            /* ctx.fillRect(0, 0, canvasWidth, canvasHeight); */
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight); 
+            addTablero();
         }
  
-        /* Agrega un circulo */
+        /* Crea una ficha */
         function addCircle(color, jug, active) {
-        let x = Math.floor(((jug == 1) ? 50 : 750) + Math.random() * 100);
-        let y = Math.floor(100 + Math.random() * 225);
-        /* let radius = Math.floor(Math.random() * 50); */
-        let fill = color;
-        let circle = new Circle(x, y, 20, fill, ctx, jug, active);
-        figures.push(circle);
+            let x = Math.floor(((jug == 1) ? 50 : 750) + Math.random() * 100);
+            let y = Math.floor(100 + Math.random() * 225);
+            /* let radius = Math.floor(Math.random() * 50); */
+            let fill = color;
+            let circle = new Circle(x, y, 20, fill, ctx, jug, active);
+            figures.push(circle);
         }
 
-
+        /* Evento que se ejecuta al hacer click en una ficha */
         function onMouseDown(event) {
-        isMouseDown = true;
-        if (ultimaFiguraClickeada != null) {
-            ultimaFiguraClickeada.setResaltado(false);
-            ultimaFiguraClickeada = null;
-        }
-        let FigClickeada = buscarFiguraClickeada(event.offsetX, event.offsetY);
-        if (FigClickeada != null) {
-            FigClickeada.setResaltado(true);
-            FigClickeada.setPosition(event.offsetX,event.offsetY);
-            ultimaFiguraClickeada = FigClickeada;
-        }  
-        drawFigure();
+            isMouseDown = true;
+            if (ultimaFiguraClickeada != null) {
+                ultimaFiguraClickeada.setResaltado(false);
+                ultimaFiguraClickeada = null;
+            }
+            let FigClickeada = buscarFiguraClickeada(event.offsetX, event.offsetY);
+            if (FigClickeada != null) {
+                FigClickeada.setResaltado(true);
+                FigClickeada.setPosition(event.offsetX,event.offsetY);
+                ultimaFiguraClickeada = FigClickeada;
+            }  
+            drawFigure();
         }
 
+        /* Funcion que retorna la ultima ficha que fue seleccionada */
         function buscarFiguraClickeada(x, y) {
         for (let i = 0; i < figures.length; i++) {
             if (figures[i].isPointInside(x, y)) {
@@ -137,6 +139,11 @@ export function PlayGame(){
         return null;
         }
 
+        /* Evento que se ejecuta cuando el click del mouse es suelto
+        * fijandose si puede ser colocado en la posicion deseada del tablero,
+        * si no es asi vuelve la ficha a su posicion original, y si puede
+        * verifica si es una ficha ganadora.
+        */
         function onMouseUp(e) {
         if (ultimaFiguraClickeada != null) {
             isMouseDown = false;
@@ -181,6 +188,7 @@ export function PlayGame(){
         }
         }
 
+        /* Evento que se ejecuta sobre la ficha cuando el mouse se mueve */
         function onMouseMove(event) {
         if (isMouseDown && ultimaFiguraClickeada != null) {
             ultimaFiguraClickeada.setPosition(event.offsetX, event.offsetY);
@@ -188,29 +196,34 @@ export function PlayGame(){
         }
         }
 
+        /* Funcion que inicializa el juego,
+        * Creando la matriz, las fichas correspondientes
+        * y agregando el tablero
+        */
         function play (){
-        for (let i = 0; i < filas; i++) {
-            board[i] = [];
-            for (let j = 0; j < columnas; j++) {
-            board[i][j] = ({ x: 0, y: 0, value: 0 });
+            for (let i = 0; i < filas; i++) {
+                board[i] = [];
+                for (let j = 0; j < columnas; j++) {
+                board[i][j] = ({ x: 0, y: 0, value: 0 });
+                }
             }
-        }
-        for (let i = 0; i < CANT_FIGURAS; i++) {
-            let img1 = new Image(40,40);
-                img1.src = "/imgGame/img12.png";
-            let img2 = new Image(40,40);
-                img2.src = "/imgGame/img11.png";
-            img1.onload = () => {
-            addFigure(/* 'green' */ img1 , 1, true);
+            for (let i = 0; i < CANT_FIGURAS; i++) {
+                let img1 = new Image(40,40);
+                    img1.src = "/imgGame/img12.png";
+                let img2 = new Image(40,40);
+                    img2.src = "/imgGame/img11.png";
+                img1.onload = () => {
+                addFigure(/* 'green' */ img1 , 1, true);
+                }
+                img2.onload = () => {
+                addFigure(/* 'blue' */  img2 , 2, false);  
+                }
             }
-            img2.onload = () => {
-            addFigure(/* 'blue' */  img2 , 2, false);  
-            }
-        }
-        deshabilitarFichas(2);
-        addTablero();
+            deshabilitarFichas(2);
+            addTablero();
         }
 
+        /* Marca las fichas del jugador pasado como parametro, para que no puedan seleccionarse */
         function deshabilitarFichas(jug) {
         figures.forEach(ficha => {
             if (ficha.jugador == jug && !ficha.isPut) {
@@ -221,6 +234,7 @@ export function PlayGame(){
         });
         }
 
+        /* Marca todas las fichas como no clickables */
         function deshabilitarTodasLasFichas(){
         figures.forEach(ficha => {
             ficha.setIsClickable(false);
@@ -229,46 +243,50 @@ export function PlayGame(){
 
         play();
 
-
+        /* Agrega el tablero del juego, colocando los casilleros donde se colocan la fichas
+        * y luego dibuja los espacios centrales donde se colocan las fichas
+        * verificando si estan ocupadas o no y colocando dichas fichas en ese caso
+        */
         function addTablero() {
-        casilleros = [];   
+            casilleros = [];   
         
-        drawTab(); 
+            drawTab(); 
 
-        for (let x = 0; x < columnas; x++) {
-            ctx.beginPath();
-            let posx = (250 + ((CANTIDADFICHAS - 6) * -1 * 20)) + x * 48;
-            ctx.arc(posx, 50, 21, Math.PI * 1, 0, true);
-            /* ctx.fillStyle = "#FFF7CE"; */
-            ctx.strokeStyle = "black";
-            ctx.lineWidth = 2;
-            /* ctx.fill(); */
-            ctx.stroke();
-            casilleros.push({ x: posx, y: 50 });
-        }
-        
-        for (let i = 0; i < filas; i++) {
-            /* board[i] = []; */
-            for (let j = 0; j < columnas; j++) {
-            /* ctx.strokeStyle = `rgb(
-                0,
-                ${Math.floor(255 - 42.5 * i)},
-                ${Math.floor(255 - 42.5 * j)})`; */
-                ctx.fillStyle = "white";
+            for (let x = 0; x < columnas; x++) {
                 ctx.beginPath();
-                let positionx = (250 + ((CANTIDADFICHAS - 6) * -1 * 20))  + j * 48;
-                let positiony = 100 + i * 48;
-                ctx.arc(positionx, positiony, 21, 0, Math.PI * 2, true);
-                ctx.fill(); //agrego fondo blanco
-                /* ctx.stroke(); */
-                /* Inicializamos la matriz de front */
-                /* board[i][j] = ({ x: positionx, y: positiony }); */
-                board[i][j].x = positionx;
-                board[i][j].y = positiony;
+                let posx = (250 + ((CANTIDADFICHAS - 6) * -1 * 20)) + x * 48;
+                ctx.arc(posx, 50, 21, Math.PI * 1, 0, true);
+                /* ctx.fillStyle = "#FFF7CE"; */
+                ctx.strokeStyle = "black";
+                ctx.lineWidth = 2;
+                /* ctx.fill(); */
+                ctx.stroke();
+                casilleros.push({ x: posx, y: 50 });
             }
+        
+            for (let i = 0; i < filas; i++) {
+                /* board[i] = []; */
+                for (let j = 0; j < columnas; j++) {
+                /* ctx.strokeStyle = `rgb(
+                    0,
+                    ${Math.floor(255 - 42.5 * i)},
+                    ${Math.floor(255 - 42.5 * j)})`; */
+                    ctx.fillStyle = "white";
+                    ctx.beginPath();
+                    let positionx = (250 + ((CANTIDADFICHAS - 6) * -1 * 20))  + j * 48;
+                    let positiony = 100 + i * 48;
+                    ctx.arc(positionx, positiony, 21, 0, Math.PI * 2, true);
+                    ctx.fill(); //agrego fondo blanco
+                    /* ctx.stroke(); */
+                    /* Inicializamos la matriz de front */
+                    /* board[i][j] = ({ x: positionx, y: positiony }); */
+                    board[i][j].x = positionx;
+                    board[i][j].y = positiony;
+                }
             }    
         }
 
+        /* Se fija si el tablero esta lleno o no, para el caso que exista empate */
         function tableroLeno(){
             for (let i = 0; i < filas; i++) {
                 for (let j = 0; j < columnas; j++) {
@@ -284,8 +302,7 @@ export function PlayGame(){
         canvas.addEventListener("mouseup", onMouseUp, false);
         canvas.addEventListener("mousemove", onMouseMove, false);
 
-        /* Verificar ganador */
-
+        /* Verifica si hay un ganador */
         function comprobarGanador(board, fila, col, filas, columnas) {
         //Mirar en todas las direcciones a partir de donde cayo la ficha para comprobar si gano
         if (verificarHorizontal(board, fila, col, columnas) || verificarAbajo(board, fila, col, filas) || verificarDiagonalIzquierda(board, fila, col, filas, columnas) || verificarDiagonalDerecha(board, fila, col, filas, columnas)){
@@ -294,6 +311,7 @@ export function PlayGame(){
         return false;
         }
 
+        /* Verifica si existen cantidad de fichas iguales a la cantidad necesaria para ganar de forma horizontal  */
         function verificarHorizontal(board, fila, col, columnas) {
         let cantFichasIguales = 0;
         while ((col < columnas-1) && (board[fila][col].value == board[fila][col+1].value)){ // verifica cuantas fichas iguales tiene a la derecha
@@ -315,6 +333,7 @@ export function PlayGame(){
         } else return false;
         }
 
+        /* Verifica si existen cantidad de fichas iguales a la cantidad necesaria para ganar de forma vertical  */
         function verificarAbajo (board, fila, col, filas){
         let cantFichasIguales = 1;
         while ((fila < filas-1) && (cantFichasIguales < CANTIDADFICHAS)) {
@@ -330,6 +349,7 @@ export function PlayGame(){
         } else return false;
         }
 
+        /* Verifica si existen cantidad de fichas iguales a la cantidad necesaria para ganar de forma diagonal derecha  */
         function verificarDiagonalDerecha(board, fila, col, filas, columnas){
         let cantFichasIguales = 0;
         while ((fila < filas-1) && (col < columnas-1) && (board[fila][col].value == board[fila+1][col+1].value)){//verifica abajo a la derecha si es igual y no se pase de la matriz
@@ -354,6 +374,7 @@ export function PlayGame(){
         } else return false;
         }
 
+        /* Verifica si existen cantidad de fichas iguales a la cantidad necesaria para ganar de forma izquierda  */
         function verificarDiagonalIzquierda(board, fila, col, filas, columnas){
         let cantFichasIguales = 0;
         while ((fila < filas-1) && (col > 0) && (board[fila][col].value==board[fila+1][col-1].value)){ //verifica abajo a la izquierda
