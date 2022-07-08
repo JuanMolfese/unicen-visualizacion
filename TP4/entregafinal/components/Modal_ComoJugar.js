db0a33af894d1c64e698ad654cea558212f057e9
JuanMolfese <juanjosemolfese@gmail.com>
2022-07-07 21:11:01 -0300

    se actualiza link a youtube , nuevo video del juego ! Se modifica MODAL como jugar

--- TP4/entregafinal/components/Modal_ComoJugar.js
+++ TP4/entregafinal/components/Modal_ComoJugar.js
@@ -19,13 +19,9 @@ export default function Modal_ComoJugar(){
             <FaTimes className={styles.iconClose} onClick={closeModal}/>
         </div>
         <div className={styles.container_detalle_como_jugar}>
-            <span className={styles.titulo_como_jugar}>Como Jugar al 4 en linea ?</span>
+            <span className={styles.titulo_como_jugar}>Como Jugar al Forest Runner ?</span>
             <br/><br/>
-            <span className={styles.textoComoJugar}> El objetivo del juego es conectar cuatro fichas de un mismo color de cualquiera de las tres formas siguientes:
-              Verticalmente, Horizontalmente y Diagonalmente.
-              Puedes jugar 4 en linea en diferentes variaciones. La cantidad de piezas necesarias para ganar puede variar. Algunas versiones requieren un tablero más grande, lo que permite juagr con cinco o incluso seis piezas.
-              Existe la posibilidad de que no todos los juegos terminen con una victoria para uno de los jugadores. Si se llenan todas las celdas del tablero y no estan la cantidad de fichas necesarias para ganar, se anuncia el empate.
-              El tiempo total de juego es de 10 minutos.              
+            <span className={styles.textoComoJugar}> El objetivo del juego es juntar 10 monedas sin morir en el intento, ya que los monstruos del bosque estaran al acecho. Utiliza la barra espaciadora para saltar. En cualquier momento del juego puedes presionar la tecla escape / esc para salir.              
             </span>
             
         </div> 
